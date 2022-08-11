1bfef42ba043a28216b4276359cc7c083c7ee669
shashi <shashidharngg@gmail.com>
2022-08-11 23:07:40 +0530

    UC1length of line

---
+++ src/com/bridgelabz/lengthoftwopoints.java
@@ -0,0 +1,14 @@
+package com.bridgelabz;
+
+public class lengthoftwopoints {
+
+	public static void main(String[] args) {
+		int x1=1,y1=1,x2=4,y2=4;
+		double length=Math.sqrt(Math.pow(x2-x1,2)+Math.pow(y2-y1,2));
+		System.out.println("Length of Two Points="+length);
+	}
+		
+
+	}
+
+
